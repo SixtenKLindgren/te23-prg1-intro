a65ae4717ed827f4d2e326c97beb4603409fa869
Sixten <sixten.lindgren@elev.ga.ntig.se>
2024-10-17 09:46:58 +0200

    for loops

---
+++ forlooplektion/forloop.py
@@ -0,0 +1,19 @@
+import random
+
+saker = ["Penna", "Bok", "Ring"]
+validchoice = ["1","2","3","4"]
+while True :
+    choice = input("Vad vill du göra?\n[1] Print\n[2] Avsluta\n[3] Lägg till sak\n[4] Random sak\n")
+    if choice in validchoice :
+        if choice == "1" :
+            for i in saker :
+                print(i)
+        if choice == "2" :
+            break
+        if choice == "3" :
+            sak = input("Vilken sak vill du lägga till?\n")
+            saker.append(sak.capitalize())
+        if choice == "4" :
+            print(saker[random.randint(0,len(saker))])
+    else :
+        print("Jag fattar inte")
